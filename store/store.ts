46e2f75696fe71ee01ae8376dc6d04f7e401898c
Eliezer Marques <neto15.cg@gmail.com>
2021-02-13 17:13:43 -0300

    Fix imports non unsed

--- store/store.ts
+++ store/store.ts
@@ -1,19 +1,19 @@
-import { applyMiddleware, createStore } from "redux";
-import createSagaMiddleware from "redux-saga";
-import { createWrapper } from "next-redux-wrapper";
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createWrapper } from 'next-redux-wrapper';
 
-import rootReducer from "./reducer";
-import rootSaga from "./saga";
+import rootReducer from './reducer';
+import rootSaga from './saga';
 
 const bindMiddleware = (middleware: any) => {
-  if (process.env.NODE_ENV !== "production") {
-    const { composeWithDevTools } = require("redux-devtools-extension");
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
     return composeWithDevTools(applyMiddleware(...middleware));
   }
   return applyMiddleware(...middleware);
 };
 
-export const makeStore = (context: any) => {
+export const makeStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const store: any = createStore(rootReducer, bindMiddleware([sagaMiddleware]));
 
